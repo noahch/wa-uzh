43ae7a3ba965d3b4e40fd491a2d755a6457c5e34
Noah Chavannes <noah.chavannes93@gmail.com>
2021-05-08 11:20:44 +0200

    worked on exercise

--- js/common.js
+++ js/common.js
@@ -37,11 +37,13 @@ function toggleNavigation(event) {
     }
 }
 
+// increases the font size
 function increaseFontSize() {
   var current = parseInt($('html').css("font-size"), 10);
   $('html').css("font-size", current + 1 + "px");
 }
 
+// decreases the font size
 function decreaseFontSize() {
   var current = parseInt($('html').css("font-size"), 10);
   if (current >= 2) {
@@ -49,14 +51,73 @@ function decreaseFontSize() {
   }
 }
 
+// Changes the aria-expanded attribute of a dropdown menu
 function changeExpanded(id) {
   var current = $('#'+ id).attr("aria-expanded");
   if (current == "true") {
     $('#'+ id).attr("aria-expanded","false");
   } else {
     $('#'+ id).attr("aria-expanded","true");
+
+    // Add EventListener to unfocus when not dropdown-item anymore
+    document.addEventListener('focusin', function(e) {
+      focusMenu(e, id);
+    }, false);
   }
 }
+
+// Unfocuses as soon as focused element is not a dropdown-item anymore
+function focusMenu(event, id){
+    if(!event.target.classList.contains('dropdown-item')){
+        $('#'+ id).attr("aria-expanded", "false");
+        $("ul.dropdown-menu.show").each(function( index ) {
+            $( this ).removeClass("show");
+        });
+        document.removeEventListener('focusin', focusMenu);
+    }
+}
+
+// Closes dropdown menus on escape key and sets aria-expaned accordingly
+document.addEventListener('keydown', (event) => {
+       if (event.key === 'Escape') {
+           const isNotCombinedKey = !(event.ctrlKey || event.altKey || event.shiftKey);
+           if (isNotCombinedKey) {
+               $("[aria-expanded='true']").each(function( index ) {
+                   $( this ).attr("aria-expanded","false");
+                });
+                $("ul.dropdown-menu.show").each(function( index ) {
+                    $( this ).removeClass("show");
+                });
+           }
+       }
+   });
+
+// Function that let's every a-tag with role=button be activated using the spacebar. Also checks that dropdown menu is activated when neccessary
+$(function() {
+  $("a[role='button']").on('keyup', function(e) {
+      if (e.which === 32) {
+        console.log("here ");
+        e.preventDefault();
+        if($(this).hasClass( "dropdown-toggle" )){
+          $(this).click();
+          $(this).dropdown('toggle');
+        }
+        window.location = e.target.href;
+      }
+  });
+});
+
+// document.addEventListener('focusin', onFocus);
+//
+// function onFocus(e) {
+//   if(!$('#nav-bar-faculties > .dropdown-menu').find(e.target).length) {
+//     // $('.dropdown-menu').fadeToggle();
+//     // $('.dropdown-menu').toggleClass("open");
+//       alert();
+//         document.removeEventListener('focusin', onFocus);
+//   }
+// }
+
 
 document.addEventListener('DOMContentLoaded', function() {
     var dropDownToggles =
