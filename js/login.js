df417560cfb997af790f4240b1212c4afde83b2a
Silvan Wehrli <silvanwehrli@Silvans-MacBook-Pro.local>
2021-05-11 10:35:37 +0200

    form validation

--- js/login.js
+++ js/login.js
@@ -25,6 +25,32 @@ function removeValidation(element) {
     element.classList.remove('is-invalid');
 }
 
+function errorElement(id, msg) {
+    var a = document.createElement('a');
+    a.href = '#' + id;
+    a.dataset.pointsTo = id;
+    a.innerHTML = msg;
+    a.classList = ['text-danger'];
+    a.setAttribute('aria-live', 'assertive');
+    return a;
+}
+
+function changeErrorList(listId, msgs) {
+    var listEl = document.getElementById(listId);
+    var li;
+
+    listEl.innerHTML = '';
+    msgs.forEach(function(m) {
+        li = document.createElement('li');
+        li.appendChild(m);
+        listEl.appendChild(li);
+    })
+
+    // make it visible
+    listEl.classList.remove('sr-only');
+    listEl.classList.add('sr-only-focusable');
+}
+
 /**
  * Validate the login form and try to log the user in
  * @param {object} event - The DOM event
@@ -34,6 +60,7 @@ function login(event) {
     event.stopPropagation();
 
     var hasError = false;
+    var errorMessages = [];
 
     var email = document.getElementById('login-email-control');
     if (email.validity.valid) {
@@ -41,23 +68,30 @@ function login(event) {
     } else if (email.validity.valueMissing) {
         setInvalid(email);
         hasError = true;
+        errorMessages.push(errorElement('login-email-control', 'Please provide an email address'));
     } else {
         setInvalid(email);
         hasError = true;
+        errorMessages.push(errorElement('login-email-control', 'Please provide a valid e-mail address'));
     }
 
     var password = document.getElementById('login-password-control');
     if (password.value.trim().length == 0) {
         setInvalid(password);
         hasError = true;
+        errorMessages.push(errorElement('login-password-control', 'Please provide a valid password'))
     } else {
         setValid(password);
     }
 
     if (hasError) {
         document.getElementById('login-error').classList.remove('d-none');
+        changeErrorList('login-error-list', errorMessages);
     } else {
         document.getElementById('login-error').classList.add('d-none');
+        document.getElementById('login-error-list').classList.remove('sr-only-focusable');
+        document.getElementById('login-error-list').classList.add('sr-only');
+
     }
 }
 
@@ -101,11 +135,13 @@ function register(event) {
     event.stopPropagation();
 
     var hasError = false;
+    var errorMessages = [];
 
     var firstName = document.getElementById('register-first-name-control');
     if (firstName.value.trim().length == 0) {
         setInvalid(firstName);
         hasError = true;
+        errorMessages.push(errorElement('register-first-name-control', 'Please provide a valid first name'))
     } else if (firstName.validity.valid) {
         setValid(firstName);
     }
@@ -114,6 +150,7 @@ function register(event) {
     if (lastName.value.trim().length == 0) {
         setInvalid(lastName);
         hasError = true;
+        errorMessages.push(errorElement('register-last-name-control', 'Please provide a valid last name'))
     } else if (lastName.validity.valid) {
         setValid(lastName);
     }
@@ -124,9 +161,11 @@ function register(event) {
     } else if (email.validity.valueMissing) {
         setInvalid(email);
         hasError = true;
+        errorMessages.push(errorElement('register-email-control', 'Please provide an email address'));
     } else {
         setInvalid(email);
         hasError = true;
+        errorMessages.push(errorElement('register-email-control', 'Please provide a valid email address'));
     }
 
     var password = document.getElementById('register-password-control');
@@ -134,15 +173,19 @@ function register(event) {
     if (passwordValue.length < 8) {
         setInvalid(password);
         hasError = true;
+        errorMessages.push(errorElement('register-password-control', 'Please provide a password that contains between 8 and 16 characters.'));
     } else if (passwordValue.length > 16) {
         setInvalid(password);
         hasError = true;
+        errorMessages.push(errorElement('register-password-control', 'Please provide a password that contains between 8 and 16 characters.'));
     } else if (passwordValue.match(/[a-zA-Z]+/) == null) {
         setInvalid(password);
         hasError = true;
+        errorMessages.push(errorElement('register-password-control', 'Password must contain letters.'));
     } else if (passwordValue.match(/[0-9]+/) == null) {
         setInvalid(password);
         hasError = true;
+        errorMessages.push(errorElement('register-password-control', 'Password must contain numbers.'));
     } else {
         setValid(password);
     }
@@ -156,12 +199,16 @@ function register(event) {
         hasError = true;
     } else {
         setValid(programme);
+        errorMessages.push(errorElement('register-programme-control', 'Please select a studdy programme'))
     }
 
     if (hasError) {
         document.getElementById('register-error').classList.remove('d-none');
+        changeErrorList('register-error-list', errorMessages);
     } else {
         document.getElementById('register-error').classList.add('d-none');
+        document.getElementById('register-error-list').classList.remove('sr-only-focusable');
+        document.getElementById('register-error-list').classList.add('sr-only');
     }
 }
 
